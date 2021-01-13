7d74ca292ae8a16c1fc956ec92473ccdd0eee793
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-13 03:58:20 +0200

    same, but with longer test

--- spec/prime_sift_spec.rb
+++ spec/prime_sift_spec.rb
@@ -13,5 +13,9 @@ describe 'prime_sift' do
   it 'gives us [2, 3, 5, 7] on 8' do
     expect(prime_sift(8)).to eq([2, 3, 5, 7])
   end
+
+  it 'correct answer for 40' do
+    expect(prime_sift(40)).to eq([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37])
+  end
 end
 
