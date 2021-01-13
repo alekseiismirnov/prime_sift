e779d781c6ad74517944fdf9f2d92bcebcea92c0
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-13 03:52:02 +0200

    pass tests, asks for refactoring

--- lib/prime_sift.rb
+++ lib/prime_sift.rb
@@ -1,3 +1,13 @@
 def prime_sift n
-  return [2]
+  range = (2..n).to_a
+  current_prime = 0
+
+  while current_prime < range.length
+    p = range[current_prime]
+
+    range.reject! {|i| ((i != p) and (i % p)) == 0}
+    current_prime += 1
+  end
+
+  return range
 end
--- spec/prime_sift_spec.rb
+++ spec/prime_sift_spec.rb
@@ -5,5 +5,13 @@ describe 'prime_sift' do
   it 'gives us [2] on 2' do
     expect(prime_sift(2)).to eq([2])
   end
+
+  it 'gives us [2, 3] on 3' do
+    expect(prime_sift(3)).to eq([2, 3])
+  end
+
+  it 'gives us [2, 3, 5, 7] on 8' do
+    expect(prime_sift(8)).to eq([2, 3, 5, 7])
+  end
 end
 
